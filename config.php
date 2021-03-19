f575f02294108bc2cede367aed26c97f13f1020d
Marvin Rabe <marvinrabe@users.noreply.github.com>
2021-03-19 12:17:38 +0100

    April

--- config.php
+++ config.php
@@ -40,13 +40,16 @@ return [
         ]
     ],
     'upcomingOnlineMeetup' => [
-        'date' => Carbon::parse('2021-03-01 20:00:00+0100'),
+        'date' => Carbon::parse('2021-04-12 20:00:00+0200'),
         'youtubeUrl' => 'https://www.youtube.com/channel/UCTXsbGt5J7bbTcC_TZcJPqQ',
         'calendarBase64' => '',
         'talks' => [],
         'sparks' => [],
     ],
     'pastOnlineMeetups' => [
+        [
+            'date' => Carbon::parse('2021-03-01 20:00:00+0100')
+        ]
         [
             'date' => Carbon::parse('2021-02-01 20:00:00+0100'),
             'youtubeUrl' => '',
