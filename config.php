e3346cef87b7219d7ae9788969031bcd22af8513
Marvin Rabe <marvinrabe@users.noreply.github.com>
2021-04-01 11:20:33 +0200

    Update config.php

--- config.php
+++ config.php
@@ -49,7 +49,7 @@ return [
     'pastOnlineMeetups' => [
         [
             'date' => Carbon::parse('2021-03-01 20:00:00+0100')
-        ]
+        ],
         [
             'date' => Carbon::parse('2021-02-01 20:00:00+0100'),
             'youtubeUrl' => '',
