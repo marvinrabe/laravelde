f89b37d0e96d29b86e4c8dcbaa034c4c8b4d9e8f
Marvin Rabe <marvin.rabe@gmail.com>
2021-04-19 17:41:30 +0200

    fix: change avatar provider.

--- components/PastMeetup.tsx
+++ components/PastMeetup.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
 
@@ -23,7 +24,8 @@ export default function PastMeetup (props: PastMeetupProps) {
 
   return (
     <div className="border border-gray-200 rounded mb-6">
-      <div className="flex flex-col md:flex-row items-center md:justify-between px-6 py-3 bg-gray-50 border-b border-gray-200 rounded-t">
+      <div
+        className="flex flex-col md:flex-row items-center md:justify-between px-6 py-3 bg-gray-50 border-b border-gray-200 rounded-t">
         <div className="text-lg font-bold text-gray-800 my-2 md:my-0">
           {new Intl.DateTimeFormat('de-DE', {
             dateStyle: 'long'
@@ -38,10 +40,14 @@ export default function PastMeetup (props: PastMeetupProps) {
       <div className="px-6 py-3">
         {talks.map((talk, index) => (
           <div key={index} className="flex items-center my-6">
-            <img src={`https://api.microlink.io/?url=https://twitter.com/${talk.twitter}&embed=image.url`}
-                 className="rounded-full shadow-lg h-12 w-12 outline-none flex-shrink-0" alt=""/>
+            <Image
+              src={`https://res.cloudinary.com/laravelphpde/image/twitter_name/${talk.twitter}.jpg`}
+              className="rounded-full shadow-lg h-12 w-12 outline-none flex-shrink-0"
+              height={50}
+              width={50}
+            />
             <div className="ml-4 leading-relaxed">
-              <a href={`https://twitter.com/${talk.twitter}`} className="font-bold" rel="noopener">{talk.name}</a><br />
+              <a href={`https://twitter.com/${talk.twitter}`} className="font-bold" rel="noopener">{talk.name}</a><br/>
               <span className="font-medium text-gray-600">{talk.title}</span>
             </div>
           </div>
